d55a28d30096f6024b04d876d0ef86081991ed55
Sai Raveendra Kandregula <sairk@cdot.in>
2024-05-28 16:44:01 +0530

    Added More config options

--- config_inc.php
+++ config_inc.php
@@ -12,8 +12,18 @@ $g_crypto_master_salt       = getenv('MASTER_SALT');
 
 
 # Configure email
+$g_phpMailer_method         = PHPMAILER_METHOD_MAIL; # or PHPMAILER_METHOD_SMTP, PHPMAILER_METHOD_SENDMAIL
+$g_smtp_host                = getenv('SMTP_HOST') !== false ? getenv('SMTP_HOST') : 'localhost';                       # used with PHPMAILER_METHOD_SMTP
+$g_smtp_port                = getenv('SMTP_PORT') !== false ? getenv('SMTP_PORT') : 25;
+$g_smtp_username            = getenv('SMTP_USERNAME') !== false ? getenv('SMTP_USERNAME') : '';                                   # used with PHPMAILER_METHOD_SMTP
+$g_smtp_username            = getenv('SMTP_USERNAME') !== false ? getenv('SMTP_USERNAME') : '';                                   # used with PHPMAILER_METHOD_SMTP
+$g_smtp_password            = getenv('SMTP_PASSWORD') !== false ? getenv('SMTP_PASSWORD') : '';
 $g_webmaster_email          = getenv('EMAIL_WEBMASTER') !== false ? getenv('EMAIL_WEBMASTER') : null;
 $g_from_email               = getenv('EMAIL_FROM') !== false ? getenv('EMAIL_FROM') : null;
 $g_return_path_email        = getenv('EMAIL_RETURN_PATH') !== false ? getenv('EMAIL_RETURN_PATH') : null;
+
+# Instance Settings
+$g_path                     = getenv('MANTIS_URL') !== false ? getenv('MANTIS_URL') : null;
+$g_short_path               = getenv('MANTIS_URL_SHORT') !== false ? getenv('MANTIS_URL_SHORT') : null;
 
 // include 'config_inc_addon.php';
